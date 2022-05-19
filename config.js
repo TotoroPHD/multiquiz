5da219e043ac38100628f11422b93db18e8881c1
TotoroPHD <64203596+TotoroPHD@users.noreply.github.com>
2022-05-19 08:17:48 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+// font affichage
+const configFont = "Berlin sans FB"
+
+// Gestion
+// Mot de passe OBS Websocket
+const OBSwsPassword = "okokok";
+// Nom du compte Twitch
+const twitchAccount = "TotoroPHD";
+// OAuth 
+const OAuth = "oauth:bdi7ytz9b78cc86skwqvk0hp6p5qdo"
+// Nom source navigateur layout
+
+
